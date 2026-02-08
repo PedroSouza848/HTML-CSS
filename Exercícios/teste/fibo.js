75dbd7ae3e6d10f4fa21bde8851a244872fb46fc
Pedro <souzapedro687@gmail.com>
2026-02-08 11:09:24 -0300

    aulas da faculdade

---
+++ Exercícios/teste/fibo.js
@@ -0,0 +1,20 @@
+
+const fibonacci = (x) => {
+    if (x==0 || x==1)
+        return 1;
+    if (x==2)
+        return 2;
+    
+    let fm1 = 1, fm2 = 1, fm;
+    for (let i = 2; i < x; i++)
+    {
+        fm = fm1 + fm2;
+        fm2 = fm1;
+        fm1 = fm;
+    }
+    return fm;
+}
+
+const fiboRec = (x) => {
+    return (x == 0 || x == 1)? 1 : (fiboRec(x-1)+fiboRec(x-2));
+}
